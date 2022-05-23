03804966b55cae209cf61942a6c10abcaa863806
Sean Aujong <seanaujong@gmail.com>
2022-05-23 15:02:28 -0500

    1 - two sum

---
+++ 1-two-sum.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/two-sum/
+
+Keep a cache of previous numbers, where the key
+is the number, and the value is its index.
+One-pass O(N) solution.
+
+Alternative solutions:
+
+O(N^2) nested loop
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        cache = dict()
+        for i in range(len(nums)):
+            n = nums[i]
+            other = target - n
+            if other in cache:
+                return [cache[other], i]
+            cache[n] = i
